f44494eeae3d8e1ff05640d6f8220b74444a30ae
Serhii Kotsar <sergejkotsar94@gmail.com>
2024-03-09 09:35:48 +0200

    HW 3 was added

---
+++ Homework_3/calculator.py
@@ -0,0 +1,22 @@
+first_number = int(input('Enter first number: '))
+second_number = int(input('Enter second number: '))
+action = input('Choose one of these operations (+, -, *, /): ')
+
+if action in ('+', '-', '*', '/'):
+    if action == '+':
+        result_value = first_number + second_number
+        print(f'The sum of two numbers is equal: {result_value}')
+    if action == '-':
+        result_value = first_number - second_number
+        print(f'The difference between the two numbers is equal: {result_value}')
+    if action == '*':
+        result_value = first_number * second_number
+        print(f'The multiplication of two numbers equals: {result_value}')
+    if action == '/':
+        if not second_number == 0:
+            result_value = first_number / second_number
+            print(f'The division of two numbers equals: {result_value}')
+        else:
+            print('You cannot divide by 0')
+else:
+    print('You should choose one of these symbols (+, -, *, /). Try again')
