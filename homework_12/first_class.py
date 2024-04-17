967242e34b0fa2988bd7524bafaabdd06d7cc819
Serhii Kotsar <sergejkotsar94@gmail.com>
2024-04-17 07:30:14 +0300

    hw 12 was added

---
+++ homework_12/first_class.py
@@ -0,0 +1,17 @@
+#  1. Create a class describing any company. For example, Toshiba or Apple
+class Company:
+
+    def __init__(self, name, logo, tagline, sphere):
+        self.name = name
+        self.logo = logo
+        self.tagline = tagline
+        self.sphere = sphere
+
+    def company_info(self):
+        return f'Company name: {self.name}. Logo: {self.logo}. Tagline: {self.tagline}. Sphere: {self.sphere}.'
+
+
+# Instantiating the Company class
+my_company = Company(name='Apple', logo='bitten apple', tagline='think different', sphere='electronics')
+
+print(my_company.company_info())
