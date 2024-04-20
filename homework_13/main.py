a7807525395716bd0106fcb6d8ae112f13f7948b
Serhii Kotsar <sergejkotsar94@gmail.com>
2024-04-20 08:42:32 +0300

    hw 13 was added

---
+++ homework_13/main.py
@@ -0,0 +1,5 @@
+from homework_13.worker_class import Worker
+
+if __name__ == '__main__':
+    first_worker = Worker(name='Ethan Winters', position='head of the branch', salary=3500, experience=8)
+    print(first_worker)
